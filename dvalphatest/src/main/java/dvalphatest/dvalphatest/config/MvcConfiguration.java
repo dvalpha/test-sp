6d038d460642f87d0c404cee3dc1ed1f03c0038e
dvalpha <archivo.millan@gmail.com>
2018-03-13 17:11:35 +0100

    test2

--- dvalphatest/src/main/java/dvalphatest/dvalphatest/config/MvcConfiguration.java
+++ dvalphatest/src/main/java/dvalphatest/dvalphatest/config/MvcConfiguration.java
@@ -1,5 +1,5 @@
 package dvalphatest.dvalphatest.config;
-//File config to access a BBDD Mysql
+//File config to access a BBDD Mysqls
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
