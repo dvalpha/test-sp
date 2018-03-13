0135c4cbbebf1ca9984af2b55b6f082a110fb950
dvalpha <archivo.millan@gmail.com>
2018-03-13 21:54:35 +0100

    upp

---
+++ dvalphatest/src/main/java/dvalphatest/dvalphatest/controller/Controlador.java
@@ -0,0 +1,22 @@
+package dvalphatest.dvalphatest.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.mockito.Mockito;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class Controlador {
+
+	@RequestMapping(value = { "/mai" })
+	public ModelAndView main(HttpServletResponse response, HttpServletRequest request) throws IOException {
+		System.out.println("Llega al controlador");
+    return new ModelAndView("/home");
+	}
+
+}
